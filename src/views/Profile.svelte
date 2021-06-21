ae4ba4e470147b23d16d74cdb85e0ca4d04ae8b2
Fikayo <fikfikky@gmail.com>
2021-06-21 20:12:21 +0100

    Profile page done

--- src/views/Profile.svelte
+++ src/views/Profile.svelte
@@ -1,3 +1,32 @@
+<script>
+  import CodeSnippet from "../components/CodeSnippet.svelte";
+  import { user } from "../store";
+
+  $: code = JSON.stringify($user, null, 2);
+</script>
+
 <main>
-  <p>Profile page</p>
+  <div class="content-layout">
+    <h1 class="content__title">Profile</h1>
+    <div class="content__body">
+      <p>
+        You can use the ID Token to get the profile information of an
+        authenticated user.
+        <br />
+        <strong>Only authenticated users can access this page.</strong>
+      </p>
+      <div class="profile-grid">
+        <div class="profile__header">
+          <img src={$user.picture} alt="Profile" class="profile__avatar" />
+          <div class="profile__headline">
+            <h2 class="profile__title">{$user.name}</h2>
+            <span class="profile__description">{$user.email}</span>
+          </div>
+        </div>
+        <div class="profile__details">
+          <CodeSnippet {code} />
+        </div>
+      </div>
+    </div>
+  </div>
 </main>
